5ba4dea28e782f5011d622a706ba5cbaf5cd799e
Ruslan Arifullin <rarifullin@expedia.com>
2019-01-02 17:05:17 +0300

    fixed ex_04/Ch.02 and ex_02/Ch.03

--- Ch.03/ex_03.cpp
+++ Ch.03/ex_03.cpp
@@ -2,8 +2,10 @@
    Т.е. строку надо закодировать в цифры
    Пример:
    Right? Yes!
-   Вывод:
+   Вывод (например):
    18,12312,171,763,98423,1208,216,11,500,18,241,0,32,20620,27,10
+
+   Корректность работы можно проверить в дешифровщике
 */
 #include <iostream>
 #include <string>
@@ -17,54 +19,60 @@ int main() {
 
     modeType mode = UPPERCASE; // по умолчанию зашивровывается по алгоритму для заглавных букв
     char inputChar = cin.get();
-        
-        
-    int countChar = 0;    
+
+
+    int countChar = 0;
     while (inputChar - 0 != 10) {
     	//cout << "Salam: " << inputChar << inputChar - 0	 << endl;
         int digit = inputChar;
-                
+
         if (inputChar >= 'a' && inputChar <= 'z') {
-            //LOWERCASE            
+            //LOWERCASE
             if (mode != LOWERCASE) {
-                outputString +=  ',' + to_string((rand() % 100 + 1) * 27);
                 mode = LOWERCASE;
+                // вывод флага-перехода к другому регистру
+                cout << ',' << (rand() % 1000) * 27;
             }
-            outputString += ((countChar == 0) ? "" : ",") + to_string(digit - 'a' + 1);
+
+            outputString = ((countChar == 0) ? "" : ",") + to_string((digit - 'a' + 1) + (rand() % 1000) * 27);
         }
+
         else if (inputChar >= 'A' && inputChar <= 'Z') {
-            //UPPERCASE            
-            if (mode != UPPERCASE) { 
-                outputString +=  ',' + to_string((rand() % 100 + 1) * 27);
+            //UPPERCASE
+            if (mode != UPPERCASE) {
                 mode = UPPERCASE;
+                // вывод флага-перехода к другому регистру
+                cout << ',' << (rand() % 1000) * 27;
             }
-            outputString += ((countChar == 0)? "" : ",") + to_string(digit - 'A' + 1);                
+
+            outputString = ((countChar == 0)? "" : ",") + to_string((digit - 'A' + 1) + (rand() % 1000) * 27);
         }
 
         else {
-            //PUNCTUATIN            
+            //PUNCTUATIN
             if (mode != PUNCTUATION){
-                outputString += ',' + to_string((rand() % 100 + 1) * 9);
                 mode = PUNCTUATION;
+                cout << ',' << to_string((rand() % 1000) * 9);
             }
-            
+
             const int ARRAY_SIZE = 8;
             const char punctuation[8] = {'!', '?', ',', '.', ' ', ';', '"', '\''};
-            
+
             int targetPos = 0;
             while (punctuation[targetPos] != inputChar & targetPos < ARRAY_SIZE)
                 targetPos++;
-                        
+
             int punctuationCode = targetPos + 1;
-                
-            outputString += ((countChar == 0)? "" : ",") + to_string(punctuationCode); 
+
+            outputString = ((countChar == 0)? "" : ",") + to_string(punctuationCode + (rand() % 1000 + 1) * 9);
         }
-        
+
         countChar++;
         inputChar = cin.get();
-    } 
-    
-    cout << outputString;
-    
+        cout << outputString;
+    }
+
+    cout << endl;
+
     return 0;
 }
