6208d3a56e4e34adcdd193113d3cc0db03c2e015
Ruslan Arifullin <rarifullin@expedia.com>
2019-01-14 17:47:46 +0300

    hw_08/Ch.03 is done

--- Ch.03/hw_08.cpp
+++ Ch.03/hw_08.cpp
@@ -1,9 +1,9 @@
-/**    
+/**
     Напишите программу, которая обрабатывает массив объектов
     student и определяет квартили оценок, то есть оценки, которые
     необходимо получить студенту, чтобы успевать также хорошо
-    или лучше, чем 25% студентов, 50% студентов и 75% студентов.    
-    
+    или лучше, чем 25% студентов, 50% студентов и 75% студентов.
+
     квартиль: определение из википедии https://en.wikipedia.org/wiki/Quartile
     технология расчета: https://stackoverflow.com/questions/43792127/compute-the-quartiles-of-a-given-list-of-numbers
     пример: https://github.com/nextgis/nextgisqgis/blob/eeb0861312272aee99bba8ed91103c5c1cb3e7de/src/core/qgsstatisticalsummary.cpp (искать  mFirstQuartile )
@@ -27,7 +27,7 @@ int compareGrade(const void * A, const void * B) {
 }
 
 // нахождение медианы
-double getMedian(student array[], int size) {
+double getMedian(student * array, int size) {
     double result = 0;
 
     // если количество элементов четное
@@ -40,12 +40,9 @@ double getMedian(student array[], int size) {
     return result;
 }
 
-
-
-int main() {    
+int main() {
     const int ARRAY_SIZE = 10;
-
-    student studentArray[ARRAY_SIZE] = {
+    student * studentArray = new student[ARRAY_SIZE]{
         {87, 10001, "Fred"},
         {28, 10002, "Tom"},
         {100, 10003, "Alistair"},
@@ -55,14 +52,26 @@ int main() {
         {75, 10007, "Leslie"},
         {70, 10008, "Candy"},
         {81, 10009, "Aretha"},
-        {68, 10010, "Veronica"},
+        {68, 10010, "Veronica"}
     };
 
     // сортировка по оценкам
-    qsort(studentArray, ARRAY_SIZE, sizeof(student), compareGrade);  
-    
-    int med = 
-    cout << getMedian(studentArray, ARRAY_SIZE);
-   
+    qsort(studentArray, ARRAY_SIZE, sizeof(student), compareGrade);
+
+    double q1 = getMedian(studentArray,
+                          ARRAY_SIZE / 2
+                          );
+
+    double q2 = getMedian(studentArray,
+                          ARRAY_SIZE
+                          );
+
+    double q3 = getMedian(studentArray,
+                          (ARRAY_SIZE + (ARRAY_SIZE / 2)));
+
+
+    cout << "25% students have " << q1 << " mark" << endl;
+    cout << "50% students have " << q2 << " mark" << endl;
+    cout << "75% students have " << q3 << " mark" << endl;
     return 0;
 }
