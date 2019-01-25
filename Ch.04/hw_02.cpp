7c6d719b4c3d1e54e8b5312fddbeff548d5523e0
Ruslan Arifullin <rarifullin@expedia.com>
2019-01-25 18:17:33 +0300

    hw_02 is edited

--- Ch.04/hw_02.cpp
+++ Ch.04/hw_02.cpp
@@ -18,15 +18,6 @@ using namespace std;
 
 typedef char * arrayString;
 
-arrayString substring(arrayString& s, int pos, int length) {
-    arrayString out = new char[length];
-    for (int i = 0; i != length; ++i) {
-        out[i] = s[i + pos - 1];
-    }
-    
-    return out;
-}
-
 char characterAt(arrayString s, int position) {
 	return s[position];
 }
@@ -35,19 +26,31 @@ int length(arrayString s){
 	int count = 0;
 	while (s[count] != 0)
 		count++;
-	
+
 	return count;
 }
 
+arrayString substring(arrayString& s, int pos, int len) {
+    pos--;
+
+    int len_sub = (pos + len > length(s)) ? (len - (pos + 1)) : len;
+
+    arrayString out = new char[len_sub];
+    for (int i = 0; i != len_sub; ++i) {
+        out[i] = s[i + pos];
+    }
+
+    return out;
+}
 
 void append(arrayString& s, char c) {
 	int oldLength = length(s);
-	
+
 	arrayString newS = new char[oldLength + 2];
 	for (int i = 0; i != oldLength; ++i) {
 		newS[i] = s[i];
 	}
-	
+
 	newS[oldLength] = c;
 	newS[oldLength + 1] = 0;
 	delete[] s;
@@ -57,20 +60,20 @@ void append(arrayString& s, char c) {
 void concatenate(arrayString &s1, arrayString &s2) {
 	int s1_oldLength = length(s1);
     int s2_Length = length(s2);
-    int s1_NewLength = s1_oldLength + s2_Length;    
+    int s1_NewLength = s1_oldLength + s2_Length;
     arrayString newS = new char[s1_NewLength + 1];
-    	
+
 	for (int i = 0; i != s1_oldLength; ++i) {
 		newS[i] = s1[i];
 	}
-    
+
 	for (int i = 0; i != s2_Length; ++i) {
 		newS[i + s1_oldLength] = s2[i];
 	}
-    	
+
 	newS[s1_NewLength] = 0;
 	delete[] s1;
-	s1 = newS;    
+	s1 = newS;
 }
 
 void concatenateTester1() {
@@ -79,7 +82,7 @@ void concatenateTester1() {
     arrayString b = new char[4];
     b[0] = 'b'; b[1] = 'e'; b[2] = 's'; b[3] = 0;
     concatenate(a, b);
-    
+
     cout << a << "\n" << endl;
 }
 
@@ -89,18 +92,24 @@ void concatenateTester2() {
     arrayString b = new char[1];
     b[0] = 0;
     concatenate(b, a);
-    
+
     cout << a << "\n" << b << endl;
-    
+
     cout << (void *)a << "\n" << (void *)b << endl;
 }
 
 void substringTester() {
-    arrayString s = "Test123";
-    cout << substring(s, 1, 4) << endl;
+    arrayString a = new char[5];
+    a[0] = 't'; a[1] = 'e'; a[2] = 's'; a[3] = 't'; a[4] = '!'; a[5] = 0;
+
+    arrayString b = new char[4];
+    b = substring(a, 1, 4);
+    cout << b << endl;
+    delete[] a;
+    delete[] b;
 }
 
 int main() {
-	substringTester();    
+	substringTester();
 	return 0;
 }
