e1a8d1f522ded815d39002d36298f8045d3a7cc8
Ruslan <ruslan.arifullin@gmail.com>
2019-02-03 16:22:16 +0300

    CH_04:hw_06 is in progress

--- Ch.04/hw_06.cpp
+++ Ch.04/hw_06.cpp
@@ -18,33 +18,102 @@ struct stringListNode {
 typedef stringListNode * arrayString;
 
 char characterAt(arrayString& s, int position) {
-    
+	arrayString as = s;
+	int count = 0;
+	
+	if (position == 0) {
+		return as->elem;
+	}
+	
+	while (as != NULL) {
+		if (count == position) {
+			return as->elem;
+		}
+		as = as->next;
+		count++;
+	}   
 
 	return 0;
 }
 
 // ---------------------- tests characterAt begin ------------
 
-void characterAtTest() {
-    
+void characterAtTest1() {
+    stringListNode * sLN0 = new stringListNode; sLN0->elem = 'a'; 
+	stringListNode * sLN1 = new stringListNode; sLN1->elem = 'b'; 
+	stringListNode * sLN2 = new stringListNode; sLN2->elem = 'c'; 
+	
+	sLN0->next = sLN1; sLN1->next = sLN2; sLN2->next = NULL;
+	arrayString as = sLN0;
+	
+	sLN0 = sLN1 = sLN2 = NULL;
+	
+	cout << characterAt(as, 0);
+	
+	delete [] as;
+}
+
+void characterAtTest2() {
+    stringListNode * sLN0 = new stringListNode; sLN0->elem = 'a'; 
+	stringListNode * sLN1 = new stringListNode; sLN1->elem = 'b'; 
+	stringListNode * sLN2 = new stringListNode; sLN2->elem = 'c'; 
+	
+	sLN0->next = sLN1; sLN1->next = sLN2; sLN2->next = NULL;
+	arrayString as = sLN0;
+	
+	sLN0 = sLN1 = sLN2 = NULL;
+	
+	cout << characterAt(as, 2);
+	
+	delete [] as;
+}
+
+void characterAtTest3() {
+    stringListNode * sLN0 = new stringListNode; sLN0->elem = 'a'; 
+	stringListNode * sLN1 = new stringListNode; sLN1->elem = 'b'; 
+	stringListNode * sLN2 = new stringListNode; sLN2->elem = 'c'; 
+	
+	sLN0->next = sLN1; sLN1->next = sLN2; sLN2->next = NULL;
+	arrayString as = sLN0;
+	
+	sLN0 = sLN1 = sLN2 = NULL;
+	
+	cout << characterAt(as, -1);
+	
+	delete [] as;
+}
+
+void characterAtTest4() {
+    stringListNode * sLN0 = new stringListNode; sLN0->elem = 'a'; 
+	stringListNode * sLN1 = new stringListNode; sLN1->elem = 'b'; 
+	stringListNode * sLN2 = new stringListNode; sLN2->elem = 'c'; 
+	
+	sLN0->next = sLN1; sLN1->next = sLN2; sLN2->next = NULL;
+	arrayString as = sLN0;
+	
+	sLN0 = sLN1 = sLN2 = NULL;
+	
+	cout << characterAt(as, 8888);
+	
+	delete [] as;
 }
 
 // ---------------------- tests characterAt end ------------
-void append(arrayString& s, char c) {
-	int oldLength = length(s);
+//void append(arrayString& s, char c) {
+	//int oldLength = length(s);
 
-	arrayString newS = new char[oldLength + 2];
-	for (int i = 0; i != oldLength; ++i) {
-		newS[i] = s[i];
-	}
+	//arrayString newS = new char[oldLength + 2];
+	//for (int i = 0; i != oldLength; ++i) {
+	//	newS[i] = s[i];
+	//}
 
-	newS[oldLength] = c;
-	newS[oldLength + 1] = 0;
-	delete[] s;
-	s = newS;
-}
+	//newS[oldLength] = c;
+	//newS[oldLength + 1] = 0;
+	//delete[] s;
+	//s = newS;
+//}
 
 int main() {
-	replaceStringTest4();
+	characterAtTest4();
 	return 0;
 }
