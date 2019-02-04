fcf9682388362b0585ddf4043af30b04abddef05
Ruslan <ruslan.arifullin@gmail.com>
2019-02-04 19:59:27 +0300

    CH_04:hw_06 is in progress

--- Ch.04/hw_06.cpp
+++ Ch.04/hw_06.cpp
@@ -99,21 +99,50 @@ void characterAtTest4() {
 }
 
 // ---------------------- tests characterAt end ------------
-//void append(arrayString& s, char c) {
-	//int oldLength = length(s);
 
-	//arrayString newS = new char[oldLength + 2];
-	//for (int i = 0; i != oldLength; ++i) {
-	//	newS[i] = s[i];
-	//}
+void append(arrayString& s, char c) {
+    stringListNode * sLN = s;
+    arrayString as = sLN;
+    while (sLN->next != NULL) {
+		sLN->next;		
+	}   
+    
+    stringListNode * sLN0 = new stringListNode; sLN0->elem = c; sLN0->next=NULL;
+    sLN->next = sLN0;
+    s = sLN;
+    
+    //sLN0 = NULL;    
+    
+    //delete [] as;
+}
 
-	//newS[oldLength] = c;
-	//newS[oldLength + 1] = 0;
-	//delete[] s;
-	//s = newS;
-//}
+// ---------------------- tests append begin ------------
+
+void appendTest1() {
+    stringListNode * sLN0 = new stringListNode; sLN0->elem = 'a'; 
+	stringListNode * sLN1 = new stringListNode; sLN1->elem = 'b'; 
+	stringListNode * sLN2 = new stringListNode; sLN2->elem = 'c'; 
+	
+	sLN0->next = sLN1; sLN1->next = sLN2; sLN2->next = NULL;
+	arrayString as = sLN0;
+	
+	sLN0 = sLN1 = sLN2 = NULL;
+	
+    char c = 'd';
+	
+    append(as, c);
+
+    while (as != NULL) {
+        cout << "ArrayString " << as->elem << endl;
+		as = as->next;		
+	}   
+   
+	delete [] as;
+}
+
+// ---------------------- tests append end ------------
 
 int main() {
-	characterAtTest4();
+	appendTest1();
 	return 0;
 }
