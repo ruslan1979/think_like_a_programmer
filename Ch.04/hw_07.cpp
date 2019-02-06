cc4a1269c396acdd4b1f575a211c494abccc9ba0
Ruslan Arifullin <rarifullin@expedia.com>
2019-02-06 15:02:07 +0300

    CH_04:hw_07 is in porgress

--- Ch.04/hw_07.cpp
+++ Ch.04/hw_07.cpp
@@ -1,5 +1,5 @@
 /* ЗАДАЧА 7:
-        В продолжение предыдущего упражнения реализуйте функцию 
+        В продолжение предыдущего упражнения реализуйте функцию
         concatenate. Не забудьте, что, если мы вызываем функцию
         concatenate(s1, s2), где оба параметра — это указатели на пер-
         вые узлы соответствующих связных списков, функция должна
@@ -144,7 +144,85 @@ void appendTest1() {
 
 // ---------------------- tests append end ------------
 
+void concatenate(arrayString& s1, arrayString& s2) {
+    stringListNode * node1 = s1;
+    stringListNode * node2 = s2;
+
+    while (node1->next != NULL) {
+		node1 = node1->next;
+	}
+
+    while (node2 != NULL) {
+        node1->next = node2;
+        //node1 = node2;
+        node2 = node2->next;
+        node1 = node1->next;
+    }
+
+}
+
+// ---------------------- tests concatenate begin ------------
+
+void concatenateTest1() {
+    stringListNode * sLN0 = new stringListNode; sLN0->elem = 'a';
+	stringListNode * sLN1 = new stringListNode; sLN1->elem = 'b';
+	stringListNode * sLN2 = new stringListNode; sLN2->elem = 'c';
+
+	sLN0->next = sLN1; sLN1->next = sLN2; sLN2->next = NULL;
+	arrayString as1 = sLN0;
+
+	sLN0 = sLN1 = sLN2 = NULL;
+
+    stringListNode * sLN5 = new stringListNode; sLN5->elem = 'd';
+	stringListNode * sLN6 = new stringListNode; sLN6->elem = 'e';
+	stringListNode * sLN7 = new stringListNode; sLN7->elem = 'f';
+
+	sLN5->next = sLN6; sLN6->next = sLN7; sLN7->next = NULL;
+	arrayString as2 = sLN5;
+
+	sLN5 = sLN6 = sLN7 = NULL;
+
+    concatenate(as1, as2);
+
+    while (as1 != NULL) {
+        cout << "ArrayString " << as1->elem << endl;
+		as1 = as1->next;
+	}
+
+	delete [] as2;
+	delete [] as1;
+}
+
+void concatenateTest2() {
+    stringListNode * sLN0 = new stringListNode;
+
+	arrayString as1 = sLN0;
+
+	//sLN0 = sLN1 = sLN2 = NULL;
+
+    stringListNode * sLN5 = new stringListNode; sLN5->elem = 'd';
+	stringListNode * sLN6 = new stringListNode; sLN6->elem = 'e';
+	stringListNode * sLN7 = new stringListNode; sLN7->elem = 'f';
+
+	sLN5->next = sLN6; sLN6->next = sLN7; sLN7->next = NULL;
+	arrayString as2 = sLN5;
+
+	sLN5 = sLN6 = sLN7 = NULL;
+
+    concatenate(as1, as2);
+
+    while (as1 != NULL) {
+        cout << "ArrayString " << as1->elem << endl;
+		as1 = as1->next;
+	}
+
+	delete [] as2;
+	delete [] as1;
+}
+
+// ---------------------- tests concatenate end ------------
+
 int main() {
-	appendTest1();
+	concatenateTest1();
 	return 0;
 }
